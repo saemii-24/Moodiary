144c3acfa63f424090a9d43d0ef96f0e09e1a060
saemii-24 <hims2419@gmail.com>
2025-11-10 14:23:11 +0900

    🔧 config: 폰트, shadcn 설정

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import FeelingLineChart from "@/components/FeelingLineChart";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="w-full max-w-4xl p-8">
-        <h1 className="text-3xl font-bold text-center mb-8 text-zinc-800 dark:text-white">
-          Moodiary
-        </h1>
-        {/* AI 문구 추가 */}
-        <p>안녕하세요 @@님. 오늘을 잘 보내고 있나요?</p>
-        <FeelingLineChart />
-      </div>
+      {/* AI 문구 추가 */}
+      <p>
+        안녕하세요 <span className="text-mood-coral">주주</span>님.
+        <br /> 행복한 11월 10일을 보내고 계신가요?
+      </p>
+      <FeelingLineChart />
     </div>
   );
 }
