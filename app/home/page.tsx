a10b081eb716938ae3cbf703262d842bdaffdbac
saemii-24 <hims2419@gmail.com>
2025-11-16 17:22:59 +0900

    🚀 feat: home 페이지

---
+++ app/home/page.tsx
@@ -0,0 +1,18 @@
+import Container from "@/components/Container";
+import FeelingCalendar from "@/components/FeelingCalendar";
+import { ArrowRight, ChevronRightIcon } from "lucide-react";
+
+export default function Home() {
+  return (
+    <Container>
+      <div>철수 님과 함께하는 오늘의 기록</div>
+      <h1>오늘 기록을 시작해볼까요?</h1>
+      <ArrowRight />
+
+      <div>
+        <h2>이번 달 기록 현황</h2>
+        <FeelingCalendar />
+      </div>
+    </Container>
+  );
+}
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,7 +3,11 @@ import Input from "@/components/ui/Input";
 
 export default function SigninPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center py-10 px-4">
+    <div className="min-h-screen flex flex-col items-center justify-center py-10 px-4">
+      <div className="text-xs text-mood-red border-mood-red p-1 px-2 rounded-full mb-3 border w-fit">
+        Welcome
+      </div>
+      <h2 className="text-3xl font-semibold -mt-1  text-mood-red">Moodiary</h2>
       <div className="w-full max-w-sm bg-white flex flex-col">
         <div className="mt-6 space-y-4">
           <Input
@@ -19,12 +23,8 @@ export default function SigninPage() {
           />
         </div>
 
-        <Button
-          variant="moodCoral"
-          size="lg"
-          className="w-full mt-4 font-medium"
-        >
-          로그인
+        <Button variant="moodRed" size="lg" className="w-full mt-4 font-medium">
+          기록 시작하기
         </Button>
       </div>
     </div>
