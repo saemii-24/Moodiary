54298f9f438ecfe482e38940bae78fca1379fd8a
saemii-24 <hims2419@gmail.com>
2025-11-26 21:39:38 +0900

    🚀 feat: post model 추가

---
+++ models/Post.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from "mongoose";
+
+const PostSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    feeling: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (_doc, ret) => {
+        return ret;
+      },
+    },
+    toObject: {
+      transform: (_doc, ret) => {
+        return ret;
+      },
+    },
+  }
+);
+
+export interface PostDocument extends mongoose.Document {
+  user: mongoose.Types.ObjectId;
+  date: Date;
+  feeling: string;
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PostModel =
+  (mongoose.models.Post as mongoose.Model<PostDocument>) ||
+  mongoose.model<PostDocument>("Post", PostSchema);
+export default PostModel;
