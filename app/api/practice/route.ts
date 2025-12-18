d904fbcfbecb5a328963c5eb7e5c902586cb0c92
saemii-24 <hims2419@gmail.com>
2025-12-18 21:01:22 +0900

    🚀 feat: 몽고db get 요청

---
+++ app/api/practice/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import { connectDB } from "@/lib/mongooose";
+
+//id로 찾기
+//id가 아닐 경우는 아래 findOne 사용함
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  await connectDB();
+  const user = await User.findById(params.id);
+  return NextResponse.json(user);
+}
+
+//email로 찾기
+export async function GET(req: Request) {
+  await connectDB();
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+
+  const user = await User.findOne({ email });
+  return NextResponse.json(user);
+}
+
+// //email 중복 체크
+// export async function GET(req: Request) {
+//   await connectDB();
+//   const { searchParams } = new URL(req.url);
+//   const email = searchParams.get("email");
+
+//   const exists = await User.exists({ email });
+//   return NextResponse.json({ exists });
+// }
+
+// //유저 조회 후 이름, 이메일만 반환
+// export async function GET() {
+//   await connectDB();
+//   const users = await User.find().select("email name");
+//   return NextResponse.json(users);
+// }
+
+// //populate
+// export async function GET() {
+//   await connectDB();
+//   const users = await User.find().populate("profile");
+//   return NextResponse.json(users);
+// }
